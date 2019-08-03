7c601906b4f4a738358f5cb238fb355e89c25755
ian <better.imhs@gmail.com>
2019-08-04 00:38:54 +0900

    Add mail app
    * this commit includes the models for mail app
    * models contain 'Customer, Marketing, Promotion'
    * Customer and Marketing are connected by Promotion

---
+++ mail/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class Marketing(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    message = models.CharField(max_length=5000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+    email = models.EmailField(blank=False, unique=True)
+    phone_number = models.CharField(max_length=15, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Promotion(models.Model):
+    marketing = models.ForeignKey(Marketing, related_name='promotions', on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, related_name='promotions', on_delete=models.CASCADE)
+    is_read = models.BooleanField(default=False)
+    sent_at = models.DateTimeField(blank=False)
