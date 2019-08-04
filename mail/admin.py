6567bcec2eaf9fd4766743ce438be2221bf0683d
ian <better.imhs@gmail.com>
2019-08-04 11:55:46 +0900

    Add Marketing, Customer, Promotion models to admin sites

--- mail/admin.py
+++ mail/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Marketing, Customer, Promotion
 
 # Register your models here.
+admin.site.register(Marketing)
+admin.site.register(Customer)
+admin.site.register(Promotion)
