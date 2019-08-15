eec15aba7142d405ee5920b2ff5f22e7d2f28802
ian <better.imhs@gmail.com>
2019-08-15 19:54:30 +0900

    Convert mail module to class

--- mail/services.py
+++ mail/services.py
@@ -3,34 +3,39 @@ from email.mime.text import MIMEText
 from email import encoders  # 파일전송을 할 때 이미지나 문서 동영상 등의 파일을 문자열로 변환할 때 사용할 패키지
 from email.mime.multipart import MIMEMultipart   # 메시지를 보낼 때 메시지에 대한 모듈
 from email.mime.base import MIMEBase     # 파일을 전송할 때 사용되는 모듈
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
 
-load_dotenv()
-MAIL_ID = os.getenv('MAIL_ID')
-MAIL_PWD = os.getenv('MAIL_PWD')
+class Mail:
 
+    def __init__(self, MAIL_ID, MAIL_PWD):
+        self.MAIL_ID = MAIL_ID
+        self.MAIL_PWD = MAIL_PWD
 
-def send_mail(service, sender, receiver, title, content):
-    service_smtp = {
-        'google': 'smtp.gmail.com',
-        'naver': 'smtp.naver.com',
-    }
-    server = smtplib.SMTP_SSL(service_smtp[service], 465)
-    server.login(MAIL_ID, MAIL_PWD)
+    def send_mail(self, service, sender, receiver, title, content):
+        service_smtp = {
+            'google': 'smtp.gmail.com',
+            'naver': 'smtp.naver.com',
+        }
+        server = smtplib.SMTP_SSL(service_smtp[service], 465)
+        server.login(self.MAIL_ID, self.MAIL_PWD)
 
-    msg = MIMEText(content)
-    msg['Subject'] = title
-    msg['From'] = sender
-    msg['To'] = receiver
+        msg = MIMEText(content)
+        msg['Subject'] = title
+        msg['From'] = sender
+        msg['To'] = receiver
 
-    server.sendmail(sender, receiver, msg.as_string())
-    server.quit()
-
+        server.sendmail(sender, receiver, msg.as_string())
+        server.quit()
 
 
 if __name__=='__main__':
+    load_dotenv(find_dotenv())
+    MAIL_ID = os.getenv('MAIL_ID')
+    MAIL_PWD = os.getenv('MAIL_PWD')
+
+    mail = Mail(MAIL_ID, MAIL_PWD)
     me = 'better.imhs@gmail.com'
     you = 'better.imhs@gmail.com'
 
-    send_mail('google', me, you, '타이틀 테스트', '내용 테스트')
+    mail.send_mail('google', me, you, '타이틀 테스트', '내용 테스트')
