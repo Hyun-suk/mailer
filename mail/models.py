1068a57dc67dcfbcffd8577a06a88817f8826a3f
ian <better.imhs@gmail.com>
2019-09-16 23:09:03 +0900

    Show marketing and customer name in admin site

--- mail/models.py
+++ mail/models.py
@@ -31,3 +31,6 @@ class Promotion(models.Model):
     is_read = models.BooleanField(default=False)
     sent_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return ( self.marketing, self.customer )
